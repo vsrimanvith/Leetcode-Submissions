00d2a0c094b7c795db54b18b125840b45fb5d846
V SRI MANVITH <60315580+vsrimanvith@users.noreply.github.com>
2023-10-05 10:30:12 -0700

    Time: 11 ms (52.62%), Space: 17.1 MB (7.64%) - LeetHub

---
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.cpp
@@ -0,0 +1,14 @@
+// Author: Sri Manvith Vaddeboyina
+
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        vector<int>ans;
+
+        for(int i = 0; i<nums.size(); i++)
+        {
+            ans.push_back(nums[nums[i]]);
+        }
+        return ans;
+    }
+};
