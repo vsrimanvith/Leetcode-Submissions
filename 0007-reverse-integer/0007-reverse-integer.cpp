931e10e0d8d6a976af52b9852598b49435c5a885
V SRI MANVITH <60315580+vsrimanvith@users.noreply.github.com>
2023-10-05 10:25:50 -0700

    Time: 5 ms (19.18%), Space: 6.3 MB (12.62%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,16 @@
+//Author: Sri Manvith Vaddeboyina
+
+class Solution {
+public:
+    int reverse(int x) {
+        int rev=0;
+        while(x!=0)
+        {
+            int k = x%10;
+            if(rev>INT_MAX/10 || rev<INT_MIN/10) return 0;
+            rev = rev*10 + k;
+            x=x/10;
+        }
+        return rev;
+    }
+};
