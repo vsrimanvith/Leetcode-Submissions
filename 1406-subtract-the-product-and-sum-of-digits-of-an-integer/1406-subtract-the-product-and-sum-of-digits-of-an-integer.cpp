515218239dced9db656da2517652ff6572093478
V SRI MANVITH <60315580+vsrimanvith@users.noreply.github.com>
2023-10-05 10:28:22 -0700

    Time: 0 ms (100%), Space: 6.6 MB (10.27%) - LeetHub

---
+++ 1406-subtract-the-product-and-sum-of-digits-of-an-integer/1406-subtract-the-product-and-sum-of-digits-of-an-integer.cpp
@@ -0,0 +1,15 @@
+//Author: Sri Manvith Vaddeboyina
+
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        int s=0,p=1;
+        while(n>0)
+        {
+            s+=n%10;
+            p*=n%10;
+            n/=10;
+        }
+        return p-s;
+    }
+};
