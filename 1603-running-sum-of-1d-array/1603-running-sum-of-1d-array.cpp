27c115c88c778af6ff2be9d7325a6ff0a353cb3c
V SRI MANVITH <60315580+vsrimanvith@users.noreply.github.com>
2023-10-05 10:29:32 -0700

    Time: 0 ms (100%), Space: 9 MB (6.19%) - LeetHub

---
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.cpp
@@ -0,0 +1,15 @@
+//Author: Sri Manvith Vaddeboyina
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int s=0;
+        vector<int>v;
+        for(int i=0; i<nums.size(); i++)
+        {
+            s += nums[i];
+            v.push_back(s);
+        }
+        return v;
+    }
+};
