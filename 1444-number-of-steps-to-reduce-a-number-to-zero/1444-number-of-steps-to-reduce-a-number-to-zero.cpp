6384f45577efe02bd14accfdd56d24305df3fe02
V SRI MANVITH <60315580+vsrimanvith@users.noreply.github.com>
2023-10-05 10:28:31 -0700

    Time: 0 ms (100%), Space: 6.2 MB (31.43%) - LeetHub

---
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -0,0 +1,13 @@
+//Author: Sri Manvith Vaddeboyina
+
+class Solution {
+public:
+int numberOfSteps(int num) {
+        int count = 0; 
+        while(num){ 
+            num = num & 1 ? num - 1 : num >> 1; 
+            count++;
+        }
+        return count;
+    }
+};
