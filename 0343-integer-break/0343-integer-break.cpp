4e6102f6f4ff053d9d21cb01773b811364795e29
V SRI MANVITH <60315580+vsrimanvith@users.noreply.github.com>
2023-10-05 23:30:48 -0700

    Time: 2 ms (49.96%), Space: 6.7 MB (17.22%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.cpp
@@ -0,0 +1,13 @@
+//Author: Sri Manvith Vaddeboyina
+
+class Solution {
+public:
+    int integerBreak(int n) {
+        if(n<=3) return n-1;
+        else{
+            if(n%3==0) return pow(3,n/3);
+            else if(n%3==1) return 4*pow(3,(n/3)-1);
+            else return pow(3,n/3)*2;
+        }
+    }
+};
