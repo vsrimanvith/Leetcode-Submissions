fbdf8e083ddfd9f92276b7278c1a334c58c11466
V SRI MANVITH <60315580+vsrimanvith@users.noreply.github.com>
2023-10-08 00:23:33 -0700

    Time: 4 ms (43.62%), Space: 6.6 MB (19.06%) - LeetHub

---
+++ 0742-to-lower-case/0742-to-lower-case.cpp
@@ -0,0 +1,11 @@
+//Author: Sri Manvith Vaddeboyina
+
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for(int i=0; i<s.size(); i++){
+            s[i]=tolower(s[i]);
+        }
+        return s;
+    }
+};
