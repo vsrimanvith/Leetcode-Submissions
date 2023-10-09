13a791d8fce1f22d003ab30dd26cb58edd8fd903
V SRI MANVITH <60315580+vsrimanvith@users.noreply.github.com>
2023-10-08 17:18:36 -0700

    Time: 8 ms (46.99%), Space: 15.5 MB (42.68%) - LeetHub

---
+++ 1651-shuffle-string/1651-shuffle-string.cpp
@@ -0,0 +1,13 @@
+//Author: Sri Manvith Vaddeboyina
+
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        string str=s;
+        for(int i=0;i<indices.size();i++)
+        {
+            str[indices[i]] = s[i];
+        }
+        return str;
+    }
+};
