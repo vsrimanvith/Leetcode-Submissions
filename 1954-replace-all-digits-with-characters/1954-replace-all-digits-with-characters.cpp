359df961af7b4dcaa851fe55031aeb46287bfef4
V SRI MANVITH <60315580+vsrimanvith@users.noreply.github.com>
2023-10-10 23:09:23 -0700

    Time: 0 ms (100%), Space: 6.6 MB (20.2%) - LeetHub

---
+++ 1954-replace-all-digits-with-characters/1954-replace-all-digits-with-characters.cpp
@@ -0,0 +1,12 @@
+//Author: Sri Manvith Vaddeboyina
+
+class Solution {
+public:
+    string replaceDigits(string s) {
+        for(int i=1;i<s.size();i=i+2)
+        {
+            s[i]=s[i-1]+(s[i]-'0');
+        }
+        return s;
+    }
+};
