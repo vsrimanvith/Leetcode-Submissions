14191960eb7e84b13a44e3b5eef18facdf0864d6
V SRI MANVITH <60315580+vsrimanvith@users.noreply.github.com>
2024-09-28 17:51:44 -0500

    Time: 0 ms (100%), Space: 7.9 MB (57.15%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,31 +1,18 @@
-//Author: Sri Manvith Vaddeboyina
-
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char>st;
-        for(int i=0;i<s.size();i++)
-        {
-            if(s[i]=='(' || s[i]=='[' || s[i]=='{')
-            {
+        stack<char> st;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[')
                 st.push(s[i]);
-            }
-
-            else if(st.empty())
-            {
-                return false;
-            }
-
-            else if((s[i]==')' && st.top()=='(') 
-                    || (s[i]==']'&&st.top()=='[') || 
-                    (s[i]=='}' && st.top()=='{'))
-            {
+            else {
+                if (st.empty() || (s[i] == ')' && st.top() != '(') ||
+                    (s[i] == '}' && st.top() != '{') ||
+                    (s[i] == ']' && st.top() != '['))
+                    return false;
                 st.pop();
             }
-            else
-            return false;
         }
         return st.empty();
-        
     }
 };
