c2e3c98ad5939fdf4d8ae97700536f779d6ae8e2
V SRI MANVITH <60315580+vsrimanvith@users.noreply.github.com>
2024-09-28 22:32:01 -0500

    Time: 3 ms (70.83%), Space: 12.4 MB (63.35%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,14 +1,14 @@
-//Author: Sri Manvith Vaddeboyina
-
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int l=0,r=nums.size()-1;
-        while(l<=r)
-        {
+        int l = 0, r = nums.size()-1;
+        while(l<=r){
             int mid=l+(r-l)/2;
-            if(nums[mid]<target) l=mid+1;
-            else r=mid-1;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(target > nums[mid]) l = mid+1;
+            else r = mid-1;
         }
         return l;
     }
