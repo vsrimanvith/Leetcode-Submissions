440c0418cf8021268c571de75aaacf8e80d6afa7
V SRI MANVITH <60315580+vsrimanvith@users.noreply.github.com>
2024-09-28 19:02:22 -0500

    Time: 11 ms (37.12%), Space: 10.1 MB (16.84%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.length()!=t.length()) return false;
-        int ch[26]={0};
+        unordered_map<char,int>mp;
+        unordered_map<char,int>mp2;
+
         for(int i=0;i<s.length();i++)
         {
-            ch[s[i]-'a']++;
+            mp[s[i]]++;
         }
-            for(int i=0;i<t.length();i++)
+        for(int i=0;i<t.length();i++)
         {
-            ch[t[i]-'a']--;
+            mp2[t[i]]++;
         }
-        for(int i=0;i<26;i++)
-        {
-            if(ch[i]>0) return false;
-        }
-        return true;
+        
+        return mp==mp2;
 
     }
 };
