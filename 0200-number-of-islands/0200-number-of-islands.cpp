b5b43928c9ed0b3e0051c7be4a493ce636f570e4
V SRI MANVITH <60315580+vsrimanvith@users.noreply.github.com>
2024-10-01 10:24:47 -0500

    Time: 34 ms (28.14%), Space: 23 MB (29.37%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -3,22 +3,21 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         int r = grid.size(), c = grid[0].size();
         int cnt=0;
-        vector<vector<int>>visited(r,vector<int>(c,0));
+        // vector<vector<int>>visited(r,vector<int>(c,0));
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
-                if (!visited[i][j] && grid[i][j] == '1') {
+                if (grid[i][j] == '1') {
                     cnt++;
-                    bfs(i, j, visited, grid);
+                    bfs(i, j, grid);
                 }
             }
         }
         return cnt;
     }
 
-    void bfs(int r, int c, vector<vector<int>>& visited, vector<vector<char>>& grid) {
+    void bfs(int r, int c, vector<vector<char>>& grid) {
         queue<pair<int, int>> q;
         q.push({r, c});
-        visited[r][c] = 1;
         int rows[4] = {-1, 0, 1, 0};
         int cols[4] = {0, 1, 0, -1};
 
@@ -32,9 +31,9 @@ public:
                 int cv = col_val + cols[i];
 
                 if (rv >= 0 && rv < grid.size() && cv >= 0 &&
-                    cv < grid[0].size() && !visited[rv][cv] &&
+                    cv < grid[0].size()&&
                     grid[rv][cv] == '1') {
-                    visited[rv][cv] = 1;
+                        grid[rv][cv] ='0';
                     q.push({rv, cv});
                 }
             }
